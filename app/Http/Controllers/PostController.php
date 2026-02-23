d354790c4006f42383820d78dc02aca49d2124b7
Maiks Deivids Feldmanis <maiks.feldmanis@gmail.com>
2026-02-23 14:57:33 +0200

    post create form with submit to store method

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class PostController extends Controller
+{
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        return $request;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PostController;
+
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', );
